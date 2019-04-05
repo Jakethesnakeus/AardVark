d5730f12a6d98ad2205f447572d5b3514f5808d3
Jakethesnakeus <jake.tangel@gmail.com>
2019-04-04 23:03:44 -0400

    Excel reader and other utilities

--- Aardvark.Utilities/SqlUtilities.cs
+++ Aardvark.Utilities/SqlUtilities.cs
@@ -1,41 +1,119 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections;
+using System.Data;
 using System.Data.SqlClient;
+using Aardvark.Core;
 
 namespace Aardvark.Utilities
 {
-    public class SqlUtilities
+    public static class DataHelperExtensions
     {
-        /// <summary>
-        /// Executes the given Query and returns a JArray of the data set.
-        /// </summary>
-        /// <param name="queryString"></param>
-        /// <returns>A JArray containing the rows of data returned.</returns>
-        public static JArray ExecuteQuery(string connectionString, string queryString)
+
+        //Open the connection
+        public static SqlConnection DBConnect(this SqlConnection sqlConnection, string connectionString)
         {
-            JArray jArray = new JArray();
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                try
-                {
-                    while (reader.Read())
-                    {
-                        JObject jObject = new JObject();
-                        for (int i = 0; i < reader.FieldCount; i++)
-                        {
-                            jObject.Add(reader.GetName(i), JToken.FromObject(reader[i]));
-                        }
-                        jArray.Add(jObject);
-                    }
-                }
-                finally
-                {
-                    reader.Close();
-                }
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                return sqlConnection;
             }
-            return jArray;
+            catch (Exception e)
+            {
+                Log.Write("ERROR :: " + e.Message);
+            }
+
+            return null;
         }
+
+
+
+        //Closing the connection 
+        public static void DBClose(this SqlConnection sqlConnection)
+        {
+            try
+            {
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Log.Write("ERROR :: " + e.Message);
+            }
+        }
+
+
+        //Execution
+        public static DataTable ExecuteQuery(this SqlConnection sqlConnection, string queryString)
+        {
+
+            DataSet dataset;
+            try
+            {
+                //Checking the state of the connection
+                if (sqlConnection == null || ((sqlConnection != null && (sqlConnection.State == ConnectionState.Closed ||
+                    sqlConnection.State == ConnectionState.Broken))))
+                    sqlConnection.Open();
+
+                SqlDataAdapter dataAdaptor = new SqlDataAdapter();
+                dataAdaptor.SelectCommand = new SqlCommand(queryString, sqlConnection);
+                dataAdaptor.SelectCommand.CommandType = CommandType.Text;
+
+                dataset = new DataSet();
+                dataAdaptor.Fill(dataset, "table");
+                sqlConnection.Close();
+                return dataset.Tables["table"];
+            }
+            catch (Exception e)
+            {
+                dataset = null;
+                sqlConnection.Close();
+                Log.Write("ERROR :: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                sqlConnection.Close();
+                dataset = null;
+            }
+
+
+        }
+
+        public static DataTable ExecuteProcWithParamsDT(this SqlConnection Conn, string procname, Hashtable parameters)
+        {
+            DataSet dataSet;
+            try
+            {
+                SqlDataAdapter dataAdaptor = new SqlDataAdapter();
+                dataAdaptor.SelectCommand = new SqlCommand(procname, Conn);
+                dataAdaptor.SelectCommand.CommandType = CommandType.StoredProcedure;
+                if (parameters != null)
+                    foreach (DictionaryEntry de in parameters)
+                    {
+                        SqlParameter sp = new SqlParameter(de.Key.ToString(), de.Value.ToString());
+                        dataAdaptor.SelectCommand.Parameters.Add(sp);
+                    }
+
+                dataSet = new DataSet();
+                dataAdaptor.Fill(dataSet, "table");
+                Conn.Close();
+                return dataSet.Tables["table"];
+            }
+            catch (Exception e)
+            {
+                dataSet = null;
+                Conn.Close();
+                Console.WriteLine("ERROR :: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                Conn.Close();
+                dataSet = null;
+            }
+        }
+
+
+
     }
 }
